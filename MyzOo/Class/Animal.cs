78b788281ae953e90dbe6082d031bb4e48d13401
GONCAVF <goncalo37865@gmail.com>
2023-11-14 15:14:02 +0000

    Add Project Classes

---
+++ MyzOo/Class/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyzOo.Class
+{
+    internal class Animal
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime dateofbirth { get; set; }
+        public string typeoffood { get; set; }
+        public bool needofmedicalchecup { get; set; }
+
+        // Family
+
+        // Jail
+
+
+    }
+}
---
+++ MyzOo/Classes/Cell.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyzOo.Classes
+{
+    internal class Cell
+    {
+        public int Id {  get; set; }
+        public bool Cleaning { get; set; }
+    }
+}
