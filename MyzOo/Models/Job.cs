88ad5e043be785085a68d949d8afe5429574907c
oliotsuke <145938169+oliotsuke@users.noreply.github.com>
2023-11-15 14:17:30 +0000

    class job and class employee added

---
+++ MyzOo/Models/Job.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyzOo.Models
+{
+    internal class Job
+    {
+        public int Id { get; set; }
+        public string role { get; set; }
+        public int salary { get; set; }
+    }
+}
---
+++ MyzOo/Models/employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyzOo.Models
+{
+    internal class Employee
+    {
+    public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public bool Checkup { get; set; }
+
+        public string Adminlevel { get; set; }  
+        public Job EmployeeJob { get; set; }
+        public Employee(int id, string name, DateTime birthday, bool checkup, string adminlevel)
+        {
+            Id = id;
+            Name = name;
+            Birthday = birthday;
+            Checkup = checkup;
+            Adminlevel = adminlevel;
+        }
+    }
+}
