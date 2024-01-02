a07b141b0d8cc0ce2d8cc2ebe124819e142cc19e
GONCAVF <goncalo37865@gmail.com>
2024-01-02 13:27:48 +0000

    Event Model

---
+++ MyzOo/Methods/EventCrud.cs
@@ -0,0 +1,138 @@
+﻿using FireSharp.Response;
+using MyzOo.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MyzOo.Methods
+{
+    internal class EventCrud
+    {
+        Firebase conn = new Firebase();
+
+        //set datas to database
+        public void SetData(string id, string name, DateTime birthday, bool isDeceased, string animalGender, int animalCell, string animalFood)
+        {
+            try
+            {
+                Event set = new Event()
+                {
+                    Id = id,
+                    Name = name,
+                    Birthday = birthday,
+                    IsDeceased = isDeceased,
+                    AnimalGender = animalGender,
+                    AnimalCell = animalCell,
+                    AnimalFood = animalFood
+                };
+                var SetData = conn.client.Set("animals/" + id, set);
+
+                MessageBox.Show($"Evento {description} Registado com sucesso", "Aviso", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error");
+            }
+        }
+
+        //Update datas
+        public void UpdateData(string id, string name, DateTime birthday, bool isDeceased, string animalGender, int animalCell, string animalFood)
+        {
+            try
+            {
+                Animal set = new Animal()
+                {
+                    Id = id,
+                    Name = name,
+                    Birthday = birthday,
+                    AnimalCell = animalCell,
+                    AnimalFood = animalFood,
+                    IsDeceased = isDeceased,
+                    AnimalGender = animalGender
+                };
+                var SetData = conn.client.Update("animals/" + id, set);
+
+                MessageBox.Show($"Animal {name} Atualizado com sucesso", "Aviso", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error");
+            }
+        }
+
+        //Delete datas
+        public void DeleteData(string id, string name)
+        {
+            try
+            {
+                var SetData = conn.client.Delete("animals/" + id);
+
+                MessageBox.Show($"Animal {name} Apagado com sucesso", "Aviso", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error");
+            }
+        }
+
+        //List of the datas
+        public List<Animal> LoadData()
+        {
+            try
+            {
+                FirebaseResponse al = conn.client.Get("animals");
+                Dictionary<string, Animal> listData = JsonConvert.DeserializeObject<Dictionary<string, Animal>>(al.Body.ToString());
+                List<Animal> allData = new List<Animal>();
+
+                if (listData != null)
+                {
+                    // verify
+                    foreach (var kvp in listData)
+                    {
+                        allData.Add(kvp.Value);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Vazio", "Aviso", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+
+                return allData;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error");
+                return null;
+            }
+        }
+
+        public Animal GetAnimal(string id)
+        {
+            try
+            {
+                var GetData = conn.client.Get("animals/" + id);
+
+                if (GetData.Body == "null")
+                {
+                    Console.WriteLine($"No data found for animal with ID {id}");
+                    return null;
+                }
+
+                Animal animal = JsonConvert.DeserializeObject<Animal>(GetData.Body.ToString());
+                return animal;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error");
+                return null;
+            }
+        }
+    }
+
+}
--- MyzOo/Models/Event.cs
+++ MyzOo/Models/Event.cs
@@ -9,9 +9,11 @@ namespace MyzOo.Models
     internal class Event
     {
         public string Id { get; set; }
-        public string Name { get; set; }
         public string Description { get; set; }
         public DateTime HourBegin { get; set; }
         public int Duration { get; set; }
+        public int NumAttendees { get; set; }
+        public string Location { get; set; }
+        public int Animal { get; set; }
     }
 }
